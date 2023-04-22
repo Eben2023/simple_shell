afb17cefd3653f130b6e7812caf07b8f50ccb291
Eben2023 <ebenpresec@yahoo.com>
2023-04-22 14:39:42 -0700

    from diff lap

--- g0.c
+++ g0.c
@@ -7,215 +7,186 @@
 
 extern char **environ;
 
-#define MAX_ARGS 64
+#define BUFSIZE 1024
 
-void print_prompt(char *cwd);
-char *read_command(char *buffer, size_t bufsize);
-char **parse_command(char *buffer);
-void execute_command(char **args, char *cwd, char *home);
-void handle_cd(char **args, char *home, char *cwd);
-void handle_exit(char **args);
-void shell_cd(char **args, char *home);
-void shell_exit(char **args);
-void shell_execute(char **args);
-void handle_exec(char **args);
-void shell_setenv(char **args);
-
-void print_prompt(char *cwd)
+int main(void)
 {
-    printf("%s$ ", cwd);
-}
+    char *buffer;
+    size_t bufsize = BUFSIZE;
+    ssize_t nread;
 
-char *read_command(char *buffer, size_t bufsize)
-{
-    ssize_t nread = getline(&buffer, &bufsize, stdin);
-    if (nread == -1)
-    {
-        if (feof(stdin))
-        {
-            printf("\n");
-            exit(EXIT_SUCCESS);
-        }
-        else
-        {
-            perror("getline error");
-            exit(EXIT_FAILURE);
-        }
-    }
-    return buffer;
-}
-
-char **parse_command(char *buffer)
-{
-    char **args = (char **)malloc(MAX_ARGS * sizeof(char *));
-    if (args == NULL)
+    buffer = (char *)malloc(bufsize * sizeof(char));
+    if (buffer == NULL)
     {
         perror("malloc error");
         exit(EXIT_FAILURE);
     }
 
-    char *token;
-    int i = 0;
-    token = strtok(buffer, " \n");
-    while (token != NULL)
+    char *cwd;
+    char *home = getenv("HOME");
+    if (home == NULL)
     {
-        args[i++] = token;
-        token = strtok(NULL, " \n");
+        perror("getenv error");
+        exit(EXIT_FAILURE);
     }
-    args[i] = NULL;
 
-    return args;
-}
-
-void execute_command(char **args, char *cwd, char *home)
-{
-    if (strcmp(args[0], "cd") == 0)
+    while (1)
     {
-        handle_cd(args, home, cwd);
-    }
-    else if (strcmp(args[0], "setenv") == 0)
-    {
-        if (args[1] == NULL || args[2] == NULL || args[3] != NULL)
+        cwd = getcwd(NULL, 0);
+        if (cwd == NULL)
         {
-            fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
-        }
-        else if (setenv(args[1], args[2], 1) == -1)
-        {
-            fprintf(stderr, "Failed to set environment variable\n");
-        }
-    }
-    else if (strcmp(args[0], "unsetenv") == 0)
-    {
-        if (args[1] == NULL || args[2] != NULL)
-        {
-            fprintf(stderr, "Usage: unsetenv VARIABLE\n");
-        }
-        else if (unsetenv(args[1]) == -1)
-        {
-            fprintf(stderr, "Failed to unset environment variable\n");
-        }
-    }
-    else if (strcmp(args[0], "exec") == 0)
-    {
-        handle_exec(args);
-    }
-    else
-    {
-        pid_t pid = fork();
-        if (pid == -1)
-        {
-            perror("fork error");
+            perror("getcwd error");
             exit(EXIT_FAILURE);
         }
-        else if (pid == 0)
+        printf("%s$ ", cwd);
+        free(cwd);
+
+        nread = getline(&buffer, &bufsize, stdin);
+        if (nread == -1)
         {
-            // child process
-            handle_exec(args);
-        }
-        else
-        {
-            // parent process
-            int status;
-            waitpid(pid, &status, 0);
-            if (WIFEXITED(status))
+            if (feof(stdin))
             {
-                int exit_status = WEXITSTATUS(status);
-                if (exit_status != 0)
-                {
-                    fprintf(stderr, "Program exited with status %d\n", exit_status);
-                }
-            }
-            else if (WIFSIGNALED(status))
-            {
-                int signal_number = WTERMSIG(status);
-                fprintf(stderr, "Program was terminated by signal %d (%s)\n", signal_number, strsignal(signal_number));
+                printf("\n");
+                exit(EXIT_SUCCESS);
             }
             else
             {
-                fprintf(stderr, "Program exited abnormally\n");
+                perror("getline error");
+                exit(EXIT_FAILURE);
             }
-        }
-    }
-}
-
-void handle_cd(char **args, char *home, char *cwd)
-{
-    shell_cd(args, home);
-}
-
-void handle_exit(char **args)
-{
-    shell_exit(NULL);
-}
-
-void handle_setenv(char **args)
-{
-    shell_setenv(args);
-}
-
-void handle_unsetenv(char **args)
-{
-    shell_unsetenv(args);
-}
-
-void handle_exec(char **args)
-{
-    execvp(args[0], args);
-    perror("execvp error");
-    exit(EXIT_FAILURE);
-}
-
-void shell_cd(char **args, char *home)
-{
-    char *dir = args[1];
-    if (dir == NULL || strcmp(dir, "~") == 0)
-    {
-        dir = home;
-    }
-    else if (strcmp(dir, "-") == 0)
-    {
-        dir = getenv("OLDPWD");
-        if (dir == NULL)
-        {
-            dir = home;
-        }
-        printf("%s\n", dir);
-    }
-    if (chdir(dir) == -1)
-    {
-        if (errno == ENOENT)
-        {
-            fprintf(stderr, "%s: Directory not found\n", dir);
         }
         else
         {
-            perror("chdir error");
-        }
-    }
-    else
-    {
-        setenv("OLDPWD", cwd, 1);
-    }
-}
-
-void shell_exit(char **args)
-{
-    int exit_status = 0;
-    if (args != NULL && args[1] != NULL)
-    {
-        exit_status = atoi(args[1]);
-    }
-    exit(exit_status);
-}
-
-void shell_setenv(char **args)
-{
-    if (args[1] == NULL || args[2] == NULL || args[3] != NULL)
-    {
-        fprintf(stderr, "Usage: if (args[1] == NULL || args[2] == NULL || args[3] != NULL")
+            char *args[BUFSIZE];
+            char *token;
+            int i = 0;
+            token = strtok(buffer, " \n");
+            while (token != NULL)
             {
-            fprintf(stderr, "Usage: %s input_file output_file\n", args[0]);
-            return 1;
+                args[i++] = token;
+                token = strtok(NULL, " \n");
             }
+            args[i] = NULL;
+
+            if (strcmp(args[0], "cd") == 0)
+            {
+                char *dir = args[1];
+                if (dir == NULL || strcmp(dir, "~") == 0)
+                {
+                    dir = home;
+                }
+                else if (strcmp(dir, "-") == 0)
+                {
+                    dir = getenv("OLDPWD");
+                    if (dir == NULL)
+                    {
+                        dir = home;
+                    }
+                    printf("%s\n", dir);
+                }
+                if (chdir(dir) == -1)
+                {
+                    if (errno == ENOENT)
+                    {
+                        fprintf(stderr, "%s: Directory not found\n", dir);
+                    }
+                    else
+                    {
+                        perror("chdir error");
+                    }
+                }
+                else
+                {
+                    setenv("OLDPWD", cwd, 1);
+                }
+            }
+            else if (strcmp(args[0], "exit") == 0)
+            {
+                int exit_status = 0;
+                if (args[1] != NULL)
+                {
+                    exit_status = atoi(args[1]);
+                }
+                exit(exit_status);
+            }
+            else if (strcmp(args[0], "setenv") == 0)
+            {
+                if (args[1] == NULL || args[2] == NULL || args[3] != NULL)
+                {
+                    fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
+                }
+                else if (setenv(args[1], args[2], 1) == -1)
+                {
+                    fprintf(stderr, "Failed to set environment variable\n");
+                }
+            }
+            else if (strcmp(args[0], "unsetenv") == 0)
+            {
+                if (args[1] == NULL || args[2] != NULL)
+                {
+                    fprintf(stderr, "Usage: unsetenv VARIABLE\n");
+                }
+                else if (unsetenv(args[1]) == -1)
+                {
+                    fprintf(stderr, "Failed to unset environment variable\n");
+                }
+                else if (strcmp(args[0], "execve") == 0)
+                {
+                    if (args[1] == NULL)
+                    {
+                        fprintf(stderr, "Usage: execve COMMAND [ARGS...]\n");
+                    }
+                    else
+                    {
+                        execve(args[1], &args[1], environ);
+                        perror("execve error");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                else
+                {
+                    pid_t pid = fork();
+
+                    if (pid == -1)
+                    {
+                        perror("fork error");
+                        exit(EXIT_FAILURE);
+                    }
+                    else if (pid == 0)
+                    {
+                        // child process
+                        execve(args[0], args, environ);
+                        perror("execve error");
+                        exit(EXIT_FAILURE);
+                    }
+                    else
+                    {
+                        // parent process
+                        int status;
+                        waitpid(pid, &status, 0);
+                        if (WIFEXITED(status))
+                        {
+                            int exit_status = WEXITSTATUS(status);
+                            if (exit_status != 0)
+                            {
+                                fprintf(stderr, "Program exited with status %d\n", exit_status);
+                            }
+                        }
+                        else if (WIFSIGNALED(status))
+                        {
+                            int signal_number = WTERMSIG(status);
+                            fprintf(stderr, "Program was terminated by signal %d (%s)\n", signal_number, strsignal(signal_number));
+                        }
+                        else
+                        {
+                            fprintf(stderr, "Program exited abnormally\n");
+                        }
+                    }
+                }
+            }
+        }
+
+        free(buffer);
+        return 0;
     }
 }
