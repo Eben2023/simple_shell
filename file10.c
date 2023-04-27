c21aed39572b0a4b1cd103e20da3ecebc049f5d1
Nonsitech <samleeu01@gmail.com>
2023-04-27 15:34:37 -0700

    file10.c

---
+++ file10.c
@@ -0,0 +1,76 @@
+#include "main.h"
+
+/**
+ * del_data_func - set dat_t to zero
+ * @info: address
+ */
+
+void del_data_func(dat_t *info)
+{
+info->arg = NULL;
+info->argv = NULL;
+info->path = NULL;
+info->argc = 0;
+}
+
+/**
+ * reg_data_func - sets struct values to 0
+ * @info: address for struct
+ * @av: para or argument
+ */
+
+void reg_data_func(dat_t *info, char **av)
+{
+int a;
+
+info->fname = av[0];
+info->argv = my_str_tw_func(info->arg, " \t");
+if (!info->argv)
+{
+info->argv = malloc(sizeof(char *) * 2);
+if (info->argv)
+{
+info->argv[0] = mystring_dup(info->arg);
+info->argv[1] = NULL;
+}
+}
+for (a = 0; info->argv && info->argv[a]; a++)
+;
+info->argc = a;
+
+checkout_alias_func(info);
+checkout_variable_func_(info);
+}
+
+/**
+ * release_data_func - frees memory and resets variables in dat_t struct
+ * @info: pointer to dat_t struct
+ * @all: flag to indicate if all memory should be freed
+ *
+ * Return: void
+ */
+void release_data_func(dat_t *info, int all)
+{
+free_func(info->argv);
+info->argv = NULL;
+info->path = NULL;
+
+if (all)
+{
+if (!info->cmd_buf)
+free(info->arg);
+
+_clear_list_func(&(info->env));
+_clear_list_func(&(info->history));
+_clear_list_func(&(info->alias));
+
+free_func(info->environ);
+info->environ = NULL;
+mem_free_func((void **)info->cmd_buf);
+
+if (info->readfd > 2)
+close(info->readfd);
+
+my_putc_char(FL_BUFFER_CONST);
+}
+}
